1d224ff20c75bd876ee5e5d85a22dc089872271e
joejs92 <joejs_92@yahoo.com>
2024-02-13 12:30:01 -0500

    lay out todo div correctly

--- src/todo.js
+++ src/todo.js
@@ -1,28 +1,40 @@
+
 function addTemplate() {
     const element = document.createElement('div');
+    element.setAttribute('class','todo');
+
+    const todoTop = document.createElement('div');
+    todoTop.setAttribute('class','todoSection');
+
+    const todoBottom = document.createElement('div');
+    todoBottom.setAttribute('class','todoSection');
 
     const title = document.createElement('INPUT');
     title.setAttribute("type", "text");
     title.setAttribute("value","Title");
-    element.appendChild(title);
+    todoTop.appendChild(title);
 
-    //The date element needs to be fixed; doesn't work.
-   /* const dueDate = document.createElement('DATE');
-    element.appendChild(dueDate); */
+    const dueDate = document.createElement('input');
+    dueDate.setAttribute("type", "date");
+    todoTop.appendChild(dueDate); 
 
     const priority = document.createElement('INPUT');
     priority.setAttribute("type", "text");
     priority.setAttribute("value","Priority");
-    element.appendChild(priority);
+    todoTop.appendChild(priority);
 
     const description = document.createElement('TEXTAREA');
     description.setAttribute("placeholder","Description");
-    element.appendChild(description); 
+    description.setAttribute("cols","52");
+    todoBottom.appendChild(description); 
 
     const editButton = document.createElement('button');
     const buttonText = document.createTextNode("Add");
     editButton.appendChild(buttonText);
-    element.appendChild(editButton);
+    todoBottom.appendChild(editButton);
+
+    element.appendChild(todoTop);
+    element.appendChild(todoBottom);
 
     return element;
 }
