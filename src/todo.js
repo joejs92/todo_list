f01e67336ac9e59112edbb920c7d620afe995a09
joejs92 <joejs_92@yahoo.com>
2024-02-14 00:03:49 -0500

    adjust todo css

--- src/todo.js
+++ src/todo.js
@@ -2,6 +2,12 @@
 function addTemplate() {
     const element = document.createElement('div');
     element.setAttribute('class','todo');
+    element.style.width = '500px';
+    element.style.padding = '4px';
+    element.style.margin='8px';
+    element.style.borderWidth = '1px';
+    element.style.borderStyle = 'solid';
+    element.style.borderColor = 'black';
 
     const todoTop = document.createElement('div');
     todoTop.setAttribute('class','todoSection');
@@ -11,7 +17,7 @@ function addTemplate() {
 
     const title = document.createElement('INPUT');
     title.setAttribute("type", "text");
-    title.setAttribute("value","Title");
+    title.setAttribute("placeholder","Title");
     todoTop.appendChild(title);
 
     const dueDate = document.createElement('input');
@@ -20,7 +26,7 @@ function addTemplate() {
 
     const priority = document.createElement('INPUT');
     priority.setAttribute("type", "text");
-    priority.setAttribute("value","Priority");
+    priority.setAttribute("placeholder","Priority");
     todoTop.appendChild(priority);
 
     const description = document.createElement('TEXTAREA');
